dcbdcd22bc858abd3ea34e8327ce2a275a7c489a
Okumura Takahiro <hfm.garden@gmail.com>
2014-02-18 18:47:03 +0900

    fix for tacahilo/puppet-xbuild#1

--- modules/xbuild/manifests/install.pp
+++ modules/xbuild/manifests/install.pp
@@ -2,9 +2,11 @@ class xbuild::install {
 
   exec { 'install xbuild':
     path    => '/usr/bin',
-    command => 'git clone git@github.com:tagomoris/xbuild.git /usr/local/xbuild',
+    command => 'git clone https://github.com/tagomoris/xbuild.git /usr/local/xbuild',
     creates => '/usr/local/xbuild',
     require => Package['git'],
   }
 
 }
+
+https://github.com/tacahilo/puppet-xbuild.git
